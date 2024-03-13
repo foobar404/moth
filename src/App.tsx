6fe3c4c0b7e7aa688b3afbd4313f7ab1de8be260
Austin Michaud <austinthemichaud@gmail.com>
2024-03-13 15:55:56 -0600

    upgrade node version

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Beta, Home } from './pages';
-import mixpanel from 'mixpanel-browser';
 import { ProtectedRoute } from './components';
 import { BrowserRouter, Routes, Route, Navigate, } from 'react-router-dom';
-
-
-mixpanel.init('d0fd76de4b27e761d97f59a0ca878094', { debug: true, track_pageview: true, persistence: 'localStorage' });
 
 
 export function App() {
--- src/components/ProtectedRoute/index.tsx
+++ src/components/ProtectedRoute/index.tsx
@@ -1,19 +1,43 @@
-import React from 'react';
-import { Beta } from '../../pages';
-import { Navigate, Outlet, Route } from 'react-router-dom';
+import { firebaseDB } from "../../index";
+import React, { useEffect, useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { collection, query, where, getDocs } from 'firebase/firestore';
 
 
 export function ProtectedRoute(props) {
     const data = useProtectedRoute();
+
+    if (data.loading) {
+        return (
+            <main>
+                Loading...
+            </main>
+        )
+    }
 
     return data.authorized ? <Outlet /> : <Navigate to="/beta" replace />;
 }
 
 
 function useProtectedRoute() {
-    const authorized = false;
+    let [loading, setLoading] = useState(true);
+    let [authorized, setAuthorized] = useState(false);
+
+    useEffect(() => {
+        checkKeyExists(localStorage.getItem("moth-beta-key"));
+    }, []);
+
+    async function checkKeyExists(keyToCheck) {
+        const betaKeysCol = collection(firebaseDB, 'beta-keys');
+        const q = query(betaKeysCol, where('key', '==', keyToCheck));
+        const querySnapshot = await getDocs(q);
+
+        if (!querySnapshot.empty) setAuthorized(true);
+        setLoading(false);
+    }
 
     return {
-        authorized
+        loading,
+        authorized,
     }
 }
